9fa3b8c354a4d4bac50cab1060a53dc6e19e4f5e
冯勇海 <2206728426@qq.com>
2018-03-19 08:52:55 +0800

    demo

--- PopupSheetDemo/PopupSheetDemo/ViewController.swift
+++ PopupSheetDemo/PopupSheetDemo/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import PopupSheet
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var strongPsvc: PopupSheetViewController?
+
+    func viewContent() -> UIView {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        view.backgroundColor = UIColor.red
+        return view
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func viewControllerContent() -> UIViewController {
+        let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "B")
+        viewController.view.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        return viewController
     }
-
-
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        var psvc: PopupSheetViewController!
+        
+        switch sender.tag {
+        case 1:
+            psvc = PopupSheetViewController.newInstance(withContent: viewContent(), direction: .up)
+            psvc.offsetView = sender
+        case 2:
+            psvc = PopupSheetViewController.newInstance(withContent: viewContent(), direction: .down)
+            psvc.offsetView = sender
+        case 3:
+            psvc = PopupSheetViewController.newInstance(withContent: viewContent(), direction: .left)
+        case 4:
+            psvc = PopupSheetViewController.newInstance(withContent: viewContent(), direction: .right)
+            psvc.offsetView = sender
+        case 5:
+            if self.strongPsvc == nil {
+                self.strongPsvc = PopupSheetViewController.newInstance(withContent: viewControllerContent(), direction: .down)
+            }
+            psvc = self.strongPsvc
+            psvc.offsetView = self.navigationController?.navigationBar
+        default:
+            psvc = PopupSheetViewController.newInstance(withContent: viewContent(), direction: .down)
+            psvc.offsetView = self.navigationController?.navigationBar
+        }
+        
+        psvc.callback = { (vc, isShow) in
+            print(vc, isShow)
+        }
+        psvc.show(in: self)
+    }
 }
-
